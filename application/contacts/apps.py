f78761c01d6aa03fd684c7f4a56e9b878f12bc53
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2024-02-23 12:57:45 +0100

    remove become partner link

--- application/contacts/apps.py
+++ application/contacts/apps.py
@@ -17,6 +17,6 @@ class ContactsConfig(OscarConfig):
     def get_urls(self):
         urls = [
             path('', self.contacts_view.as_view(), name='contacts'),
-            path('become-our-partner', self.become_our_partner.as_view(), name='become-our-partner'),
+            # path('become-our-partner', self.become_our_partner.as_view(), name='become-our-partner'),
         ]
         return self.post_process_urls(urls)
