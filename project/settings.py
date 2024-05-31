9ee29f64809e3f2f0de679dc55b4238c36efcb8d
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2024-05-31 20:10:03 +0200

    fix

--- project/settings.py
+++ project/settings.py
@@ -116,7 +116,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -379,10 +379,10 @@ LOGIN_REDIRECT_URL = reverse_lazy('catalogue:index')
 
 OSCAR_ALLOW_ANON_CHECKOUT = True
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 CORS_ALLOW_CREDENTIALS = True
-# CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 # CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
