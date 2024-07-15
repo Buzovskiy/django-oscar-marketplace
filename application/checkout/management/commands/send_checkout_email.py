ff083680b8c48bc4cf94e4aabd28cf5b83def540
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2024-07-15 17:43:10 +0200

    add send email command

---
+++ application/checkout/management/commands/send_checkout_email.py
@@ -0,0 +1,22 @@
+import stripe
+from django.template.loader import render_to_string
+from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
+from django.conf import settings
+from django.db.models import Sum
+from oscar.core.loading import get_class, get_model
+from application.catalogue.models import Product
+
+Order = get_model('order', 'Order')
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        send_mail(
+            "Subject here",
+            "Here is the message.",
+            "kidsweestep@gmail.com",
+            ["buzovskiy.v@gmail.com"],
+            fail_silently=False,
+        )
